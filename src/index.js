dc6e7a9cb05f74f91dc6b44df670276526902b77
Uraraka-Chan <71365133+Uraraka-Chan@users.noreply.github.com>
2021-07-07 16:04:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-var rpc = require("discord-rpc");
+const rpc = require("discord-rpc");
+const rpc_framework = require("./Struct/main.js");
 const client = new rpc.Client({ transport: 'ipc' });
-
+new rpc_framework(
+  client,
+  "",
+  "",
+  "",
+  "",
+  "",
+  [],
+  20000
+).start();
 
 client.login({ clientId : process.env.clientID })
 .catch(console.error);
