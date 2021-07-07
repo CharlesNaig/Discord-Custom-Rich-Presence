f2fdbad1e5006e4538ee011a24a54c4638fed159
Uraraka-Chan <71365133+Uraraka-Chan@users.noreply.github.com>
2021-07-07 16:43:53 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,24 +18,36 @@ require('dotenv').config({ path: '../.env' });
  */
 new rpc_framework(
   // Leave the client as it is.
-  client,           
-  {start: 1, end: 1},
+  
+  client,                 // RPC Client.
+  
+  {start: 1, end: 1},     // TimeStamp is the Time when you started playing the game and when it ended.
+  
   // Details and Text are strings but can also be changed to an Array so they will be randomized each interval.
-  "Test RPC Details",
-  "default",
-  "Test RPC Large Text",
-  "default",
-  "Test RPC Small Text",
+  
+  "Test RPC Details",     // Main Text.
+      
+  "default",              // Large image you must upload to the assets of your application.
+  
+  "Test RPC Large Text",  // Text of the Large Image.
+  
+  "default",              // Little image you must upload to the assets of your application.
+  
+  "Test RPC Small Text",  // Text of the Small Image.
+  
   // Buttons.
+  
   [
     {
-    label : "Test Button" ,
-    url : "https://saya.gg/invite"
+    label : "Test Button" ,         // Title of the Button.
+    url : "https://saya.gg/invite"  // Place where button will take you.
     }
   ],
+  
   // Interval will update the Presence this amount os Miliseconds.
+  
   10000
 ).start();
 
-client.login({ clientId : "788233833731457105" })
+client.login({ clientId : "YourID" })
 .catch(console.error);
