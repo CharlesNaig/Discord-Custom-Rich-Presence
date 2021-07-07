466b1856513593c334b9ea8eca2ff77f99670bbd
Uraraka-Chan <71365133+Uraraka-Chan@users.noreply.github.com>
2021-07-07 16:15:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const rpc = require("discord-rpc");
 const rpc_framework = require("./Struct/main.js");
-const client = new rpc.Client({ transport: 'ipc' });
+const client = new rpc.Client({ transport: 'ipc' });client, timestamps, details, large_image, large_text, small_image, small_text, buttons, interval
+
+/**
+ * Data for the RPC Client.
+ * @param {client} client 
+ * @param {Object} timestamps
+ * @param {string} [string{}] details
+ * @param {string} [string{}] large_text
+ * @param {string} [string{}] large_image
+ * @param {string} [string{}] large_text
+ * @param {string} [string{}] small_image
+ * @param {string} [string{}] small_text 
+ * @param {Object[]} buttons
+ * @param {number} interval
+ */
 new rpc_framework(
-  client,
-  "",
-  "",
-  "",
-  "",
-  "",
+  // Leave the client as it is.
+  client,           
+  {start: 0, end: 0},
+  // Details and Text are strings but can also be changed to an Array so they will be randomized each interval.
+  "Test RPC Details",
+  "https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png",
+  "Test RPC Large Text",
+  "https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png",
+  "Test RPC Small Text",
+  // Buttons.
   [],
+  // Interval will update the Presence this amount os Miliseconds.
   20000
 ).start();
 
